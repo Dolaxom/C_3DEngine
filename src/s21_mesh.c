0894ac07f6dda24d5f1563d5a0d340d2e3f3c513
TheDolaxom <51160711+Dolaxom@users.noreply.github.com>
2022-10-11 15:57:54 +0300

    [test][plane] Adding plane output

--- src/s21_mesh.c
+++ src/s21_mesh.c
@@ -3,9 +3,12 @@
 
 Mesh plane() {
     Mesh plane;
-    plane.polygons = malloc(2 * sizeof(plane));
+    plane.polygons = malloc(2 * sizeof(Polygon));
+    if (plane.polygons == NULL) {
+        exit(1);    
+    }
 
-    vec3D points[4] = { {-1, 0, 1}, {1, 0, 1}, {-1, 0, -1}, {1, 0, -1} };
+    vec3D points[4] = { {-0.5, 0, 0.5}, {0.5, 0, 0.5}, {-0.5, 0, -0.5}, {0.5, 0, -0.5} };
 
     plane.polygons[0].points[0] = points[1];
     plane.polygons[0].points[1] = points[3];
@@ -19,40 +22,36 @@ Mesh plane() {
 }
 
 void display() {
-    Mesh aboba = plane();
-
-    // printf("%f %f %f")
+    Mesh mesh = plane();
 
     glClearColor(0.0, 1.0, 0.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    glBlendFunc(GL_ONE, GL_ZERO);
+    glEnable(GL_BLEND);
+    // Сглаживание линий
+    glEnable(GL_LINE_SMOOTH);               
+    glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
 
-    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     glPointSize(10);
-    //GL_TRIANGLE_STRIP
-    glBegin(GL_TRIANGLE_STRIP);
+    glBegin(GL_TRIANGLES);
         glColor3f(0, 0, 0);
         
-        glVertex3f(aboba.polygons[0].points[0].x, aboba.polygons[0].points[0].y, aboba.polygons[0].points[0].z);
-        glVertex3f(aboba.polygons[0].points[1].x, aboba.polygons[0].points[1].y, aboba.polygons[0].points[1].z);
-        glVertex3f(aboba.polygons[0].points[2].x, aboba.polygons[0].points[2].y, aboba.polygons[0].points[2].z);
+        glVertex3f(mesh.polygons[0].points[0].x, mesh.polygons[0].points[0].z, mesh.polygons[0].points[0].y);
+        glVertex3f(mesh.polygons[0].points[1].x, mesh.polygons[0].points[1].z, mesh.polygons[0].points[1].y);
+        glVertex3f(mesh.polygons[0].points[2].x, mesh.polygons[0].points[2].z, mesh.polygons[0].points[2].y);
 
-        glVertex3f(aboba.polygons[1].points[0].x, aboba.polygons[0].points[0].y, aboba.polygons[0].points[0].z);
-        glVertex3f(aboba.polygons[1].points[1].x, aboba.polygons[0].points[1].y, aboba.polygons[0].points[1].z);
-        glVertex3f(aboba.polygons[1].points[2].x, aboba.polygons[0].points[2].y, aboba.polygons[0].points[2].z);
-
-        // glVertex3f(-0.5, 0.5, 0);
-
-        // glVertex3f(0.5, 0.5, 0);
-
-        // glVertex3f(-0.5, -0.5, 0);
-        
-        // glVertex3f(0.5, -0.5, 0);
+        glVertex3f(mesh.polygons[1].points[0].x, mesh.polygons[1].points[0].z, mesh.polygons[1].points[0].y);
+        glVertex3f(mesh.polygons[1].points[1].x, mesh.polygons[1].points[1].z, mesh.polygons[1].points[1].y);
+        glVertex3f(mesh.polygons[1].points[2].x, mesh.polygons[1].points[2].z, mesh.polygons[1].points[2].y);
 
     glEnd();
 
 
     glutSwapBuffers();
+
+    free(mesh.polygons);
 }
 
 int main(int argc, char **argv) {
