2bf636c366822c251f1b58f01484511642ccc7ef
Dolaxom <dolaxom4@gmail.com>
2022-11-03 20:48:50 +0300

    [world] Adding camera output

--- src/s21_world.c
+++ src/s21_world.c
@@ -4,13 +4,8 @@
 #include "s21_camera.h"
 
 int main(int argc, char **argv) {
-  vector test = {5, 2, 3, 4};
-  matrix4x4 test_matrix = {
-      {{2, 5, 3, 4}, {5, 3, 1, 2}, {1, 2, 6, 2}, {6, 2, 1, 5}}};
-
-  vector result;
-  s21_mult_matrix_on_vector(&test_matrix, &test, &result);
-  printf("%f %f %f %f", result.x, result.y, result.z, result.w);
+  fleeglut_init(argc, argv);
+  glutMainLoop();
 
   return S21_OK;
 }
