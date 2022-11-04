2eebc699150487fce1ffb74ff0a215f5af0fba6b
Dolaxom <dolaxom4@gmail.com>
2022-11-04 12:16:09 +0300

    [s21_scale] Adding mesh->scale support

--- src/math/s21_engine_math.c
+++ src/math/s21_engine_math.c
@@ -2,7 +2,8 @@
 
 void s21_scale(mesh_t *mesh, float new_x, float new_y, float new_z) {
   vector vector_new_size = { new_x, new_y, new_z, 1.0 };
+  mesh->scale = vector_new_size;
   for (int polygon = 0; polygon < mesh->count_of_polygons; polygon++) {
-    s21_calculate_scale_matrix(vector_new_size, mesh->polygons[polygon]);
+    s21_calculate_scale_matrix(mesh->scale, mesh->polygons[polygon]);
   }
 }
