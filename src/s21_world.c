10b5dc77df46772afe77eb024b13825eb14577e7
Dolaxom <dolaxom4@gmail.com>
2022-11-04 12:32:55 +0300

    [world] Move headers to .h file

--- src/s21_world.c
+++ src/s21_world.c
@@ -1,8 +1,5 @@
 #include "s21_world.h"
 
-#include "math/s21_matrix4x4.h"
-#include "s21_camera.h"
-
 int main(int argc, char **argv) {
   fleeglut_init(argc, argv);
   glutMainLoop();
--- src/s21_world.h
+++ src/s21_world.h
@@ -2,5 +2,8 @@
 #define SRC_S21_WORLD_H_
 
 #include "s21_engine.h"
+#include "math/s21_matrix4x4.h"
+#include "s21_mesh.h"
+#include "s21_camera.h"
 
 #endif  // SRC_S21_WORLD_H_
