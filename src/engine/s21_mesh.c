142b3b04d02229a0bf21be10f2e9dcff658b0b2f
Dolaxom <dolaxom4@gmail.com>
2022-11-07 00:40:05 +0300

    [mesh.c] refactoring code. Adding new vars

--- src/engine/s21_mesh.c
+++ src/engine/s21_mesh.c
@@ -27,9 +27,9 @@ mesh_t parse_obj_file(char *path_to_file) {
       memset(coordinate_y, 0, 12);
       memset(coordinate_z, 0, 12);
       memset(point, 0, 12);
+      size_t i = 2;
       if (str_from_file[0] == 'v') {
         points_count++;
-        size_t i = 0;
         // parse X
         for (i = 2; i < strlen(str_from_file);) {
             if (str_from_file[i] == ' ') {
@@ -65,6 +65,7 @@ mesh_t parse_obj_file(char *path_to_file) {
         float tmp_x = atof(coordinate_x);
         float tmp_y = atof(coordinate_y);
         float tmp_z = atof(coordinate_z);
+        
         tmp_array_of_points = realloc(tmp_array_of_points, points_count * sizeof(vector));
         if (tmp_array_of_points == NULL) {
             exit(S21_REALLOC);
@@ -74,8 +75,9 @@ mesh_t parse_obj_file(char *path_to_file) {
         tmp_array_of_points[points_count - offset_array].x = tmp_x;
         tmp_array_of_points[points_count - offset_array].y = tmp_y;
         tmp_array_of_points[points_count - offset_array].z = tmp_z;
+        tmp_array_of_points[points_count - offset_array].w = 1;
+
       } else if (str_from_file[0] == 'f') {
-        size_t i = 2;
         polygons_count++;
         int *points_tmp = NULL;
         int counts = 0;
@@ -86,10 +88,12 @@ mesh_t parse_obj_file(char *path_to_file) {
             index++;
             i++;
           }
+
           points_tmp = realloc(points_tmp, (counts + 1) * sizeof(int));
           if (points_tmp == NULL) {
               exit(S21_REALLOC);
           }
+
           points_tmp[counts] = atoi(point);
           memset(point, 0, 12);
           counts++;
@@ -99,28 +103,30 @@ mesh_t parse_obj_file(char *path_to_file) {
           index = 0;
         }
 
+        int offset_array = 1;
         result_mesh.polygons = realloc(result_mesh.polygons, polygons_count * sizeof(polygons_t));
         if (result_mesh.polygons == NULL) {
             exit(S21_REALLOC);
         }
-        result_mesh.polygons[polygons_count - 1].points = (vector*)malloc(counts * sizeof(vector));
-        if (result_mesh.polygons[polygons_count - 1].points == NULL) {
+
+        result_mesh.polygons[polygons_count - offset_array].points = (vector*)malloc(counts * sizeof(vector));
+        if (result_mesh.polygons[polygons_count - offset_array].points == NULL) {
           exit(S21_MALLOC);
         }
+
         for (int z = 0; z < counts; z++) {
-          result_mesh.polygons[polygons_count - 1].points[z] = tmp_array_of_points[points_tmp[z] - 1];
+          result_mesh.polygons[polygons_count - offset_array].points[z] = tmp_array_of_points[points_tmp[z] - offset_array];
         }
-        result_mesh.polygons[polygons_count - 1].count_of_points = counts;
+
+        result_mesh.polygons[polygons_count - offset_array].count_of_points = counts;
         free(points_tmp);
       }
     }
     fclose(file);
   } else {
-    printf("\n%d\n", S21_FILE);
     exit(S21_FILE);
   }
   result_mesh.count_of_polygons = polygons_count;
   free(tmp_array_of_points);
-  printf("\nReading file: success\n");
   return result_mesh;
 }
