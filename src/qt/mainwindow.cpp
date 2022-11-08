61ce945c29a66284a510e228b83775df4586cdd0
vvoidcat <ekaterina.akchurina98@gmail.com>
2022-11-08 04:17:49 +0300

    ui upd

--- src/qt/mainwindow.cpp
+++ src/qt/mainwindow.cpp
@@ -13,3 +13,48 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_leftb_pressed()
+{
+
+}
+
+void MainWindow::on_upb_pressed()
+{
+
+}
+
+void MainWindow::on_rightb_pressed()
+{
+
+}
+
+void MainWindow::on_downb_pressed()
+{
+
+}
+
+void MainWindow::on_autoc_clicked(bool checked)
+{
+
+}
+
+void MainWindow::on_autoc_stateChanged(int arg1)
+{
+
+}
+
+void MainWindow::on_meshd_activated(int index)
+{
+
+}
+
+void MainWindow::on_meshd_currentIndexChanged(int index)
+{
+
+}
+
+void MainWindow::on_meshd_currentTextChanged(const QString &arg1)
+{
+
+}
+
--- src/qt/mainwindow.h
+++ src/qt/mainwindow.h
@@ -15,6 +15,25 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_leftb_pressed();
+
+    void on_upb_pressed();
+
+    void on_rightb_pressed();
+
+    void on_downb_pressed();
+
+    void on_autoc_clicked(bool checked);
+
+    void on_autoc_stateChanged(int arg1);
+
+    void on_meshd_activated(int index);
+
+    void on_meshd_currentIndexChanged(int index);
+
+    void on_meshd_currentTextChanged(const QString &arg1);
+
 private:
     Ui::MainWindow *ui;
 };
